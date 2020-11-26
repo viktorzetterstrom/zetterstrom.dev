61cc17d32a8708a26f2c84d1ec35163a3f886476
Viktor Zetterström <viktor.zetterstrom@footway.com>
2020-11-26 17:17:44 +0100

    prettier reformat

--- shl/api/test/shl-res-formatter.test.js
+++ shl/api/test/shl-res-formatter.test.js
@@ -1,16 +1,16 @@
-const formatter = require('../src/response-formatter/shl-res-formatter');
+const formatter = require("../src/response-formatter/shl-res-formatter");
 
-describe('standings formatter', () => {
-  it('appends the team name to the correct teams', () => {
+describe("standings formatter", () => {
+  it("appends the team name to the correct teams", () => {
     const teams = [
-      { team: { id: 'LHF' } },
-      { team: { id: 'FBK' } },
-      { team: { id: 'LHC' } },
+      { team: { id: "LHF" } },
+      { team: { id: "FBK" } },
+      { team: { id: "LHC" } },
     ];
     const expected = [
-      { team: { id: 'LHF', name: 'Luleå' } },
-      { team: { id: 'FBK', name: 'Färjestad' } },
-      { team: { id: 'LHC', name: 'Linköping' } },
+      { team: { id: "LHF", name: "Luleå" } },
+      { team: { id: "FBK", name: "Färjestad" } },
+      { team: { id: "LHC", name: "Linköping" } },
     ];
     expect(formatter.standings(teams)).toStrictEqual(expected);
   });
