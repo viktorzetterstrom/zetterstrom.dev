8d40154efb68dacda0871a3d6db06d9148e5b7d2
Viktor Zetterström <viktor.zetterstrom@footway.com>
2020-11-30 13:01:24 +0100

    add old trivia api implementation

---
+++ trivia/api/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const trivia = require("./trivia");
+
+const app = express();
+app.use(require("helmet")());
+if (process.env.NODE_ENV === "development") app.use(require("cors")());
+
+app.get("/questions", async ({ query }, res) => {
+  const options = Object.keys(query).length > 0 ? query : { amount: 10 };
+  const questions = await trivia.service.questions(options);
+
+  res.json(questions);
+});
+
+const PORT = 5001;
+app.listen(PORT, () => console.log(`trivia api listening on ${5001}`));
