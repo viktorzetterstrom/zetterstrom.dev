0eb238804dedf050761a156d36cd5b9fa6eca306
Viktor Zetterström <viktor.zetterstrom@footway.com>
2020-12-31 20:29:59 +0100

    movies: send ratings

--- movies/src/app.ts
+++ movies/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import { getRatings } from "./ratings";
 
 const app = express();
 
 app.get("/", async (_, res) => {
-  res.send("hejsan");
+  const ratings = await getRatings();
+  res.send(ratings);
 });
 
 export { app };
