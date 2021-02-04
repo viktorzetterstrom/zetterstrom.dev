85e8b54b04282815e2cc5fcc368540325a39210b
Viktor Zetterström <viktor.zetterstrom@footway.com>
2021-02-04 09:53:16 +0100

    shl: handle lower case ids for special logo widths

--- shl/client/src/components/TeamLogo.js
+++ shl/client/src/components/TeamLogo.js
@@ -2,13 +2,17 @@ import React from "react";
 
 const specialWidths = { fbk: "34", dif: "25", lhc: "36", hv71: "41" };
 
-const TeamLogo = ({ id, name }) => (
-  <img
-    height="30"
-    width={specialWidths[id] || "30"}
-    src={`/img/${id.toLowerCase()}-30.png`}
-    alt={`${name} logo`}
-  />
-);
+const TeamLogo = ({ id, name }) => {
+  const loweredId = id.toLowerCase();
+
+  return (
+    <img
+      height="30"
+      width={specialWidths[loweredId] || "30"}
+      src={`/img/${loweredId}-30.png`}
+      alt={`${name} logo`}
+    />
+  );
+};
 
 export default TeamLogo;
