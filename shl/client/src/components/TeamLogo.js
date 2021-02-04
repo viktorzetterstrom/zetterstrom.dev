2634d557e91aef28ddddde02f06c0a007e5354e7
Viktor Zetterström <viktor.zetterstrom@footway.com>
2021-02-04 09:46:54 +0100

    shl: handle special widths of team logos

--- shl/client/src/components/TeamLogo.js
+++ shl/client/src/components/TeamLogo.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+const specialWidths = { fbk: "34", dif: "25", lhc: "36", hv71: "41" };
+
 const TeamLogo = ({ id, name }) => (
   <img
-    width="30"
     height="30"
+    width={specialWidths[id] || "30"}
     src={`/img/${id.toLowerCase()}-30.png`}
     alt={`${name} logo`}
   />
