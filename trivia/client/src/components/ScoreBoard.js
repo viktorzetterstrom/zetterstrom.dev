76908e3da8728680665da9262a10b1eb2c6b327d
Viktor Zetterström <viktor.zetterstrom@footway.com>
2021-02-25 14:02:48 +0100

    trivia: add animation when scoring points

--- trivia/client/src/components/ScoreBoard.js
+++ trivia/client/src/components/ScoreBoard.js
@@ -1,12 +1,34 @@
-import React from "react";
-import { Box, Typography } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { Box, makeStyles, Typography } from "@material-ui/core";
 
-const ScoreBoard = ({ score }) => (
-  <Box m={4}>
-    <Typography variant="h2" align="center">
-      Score: {score}
-    </Typography>
-  </Box>
-);
+const useStyles = makeStyles((theme) => ({
+  score: {
+    color: ({ celebrate }) =>
+      celebrate ? "#00ff00" : theme.palette.text.primary,
+    textShadow: ({ celebrate }) =>
+      celebrate ? "0 0 2px #00ff00, 0 0 5px #00ff00" : null,
+    transition: theme.transitions.create(),
+  },
+}));
+
+const ScoreBoard = ({ score }) => {
+  const [celebrate, setCelebrate] = useState(false);
+  const classes = useStyles({ celebrate });
+
+  useEffect(() => {
+    if (score > 0) {
+      setCelebrate(true);
+      setTimeout(() => setCelebrate(false), 500);
+    }
+  }, [score]);
+
+  return (
+    <Box m={4}>
+      <Typography variant="h2" align="center" className={classes.score}>
+        Score: {score}
+      </Typography>
+    </Box>
+  );
+};
 
 export default ScoreBoard;
