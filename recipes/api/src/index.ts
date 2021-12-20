d946eb4fd6e46c0ae456daeeb401ee81e28e06e3
Viktor Zetterström <viktor.zetterstrom@footway.com>
2021-12-20 15:53:34 +0100

    recipes: initial commit

---
+++ recipes/api/src/index.ts
@@ -0,0 +1,3 @@
+import { app } from "./app";
+
+app.listen(11001, () => "recipe-api live on port 11001");
---
+++ recipes/client/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <div>hejhej</div>;
+};
+
+export default Home;
