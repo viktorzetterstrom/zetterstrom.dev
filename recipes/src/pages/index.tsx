e8911691e41bfce678100cdd3b93855c4adfe10f
Viktor Zetterström <viktor.zetterstrom@footway.com>
2022-01-12 16:26:59 +0100

    recipes: add landing page, add more recipes

--- recipes/src/pages/index.tsx
+++ recipes/src/pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>hejhej</div>;
+  return (
+    <div>
+      <div>Welcome to my personal recipe collection.</div>
+      <div>Work in progress.👷‍♂️</div>
+    </div>
+  );
 };
 
 export default Home;
