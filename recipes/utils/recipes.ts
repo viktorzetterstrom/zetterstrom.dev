0873392b8221922ac9665738d893b8ff3ab7748c
Viktor Zetterström <viktor.zetterstrom@footway.com>
2022-01-24 09:54:27 +0100

    recipes: remove duplicate keeping of recipe slug, add error handling and validation during recipe parsing

--- recipes/utils/recipes.ts
+++ recipes/utils/recipes.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import yaml from "js-yaml";
+import Joi from "joi";
 
 export type Recipe = {
   name: string;
@@ -8,12 +9,41 @@ export type Recipe = {
   instruction: string;
 };
 
+const recipeSchema = Joi.object<Recipe>({
+  name: Joi.string().min(2).max(30).required(),
+  tags: Joi.array().items(Joi.string()).required(),
+  slug: Joi.string().min(2).max(30).required(),
+  instruction: Joi.string().required(),
+});
+
 const basepath = "./recipes";
 const fileNames = fs.readdirSync(basepath);
 
-const recipes = fileNames.map((name) =>
-  yaml.load(fs.readFileSync(`${basepath}/${name}`, "utf-8"))
-) as Recipe[];
+const readRawRecipeFromFile = (name: string) =>
+  fs.readFileSync(`${basepath}/${name}`, "utf-8");
+
+const readRecipeFromFile = (name: string): Recipe | void => {
+  try {
+    const rawRecipe = yaml.load(readRawRecipeFromFile(name));
+    if (typeof rawRecipe !== "object") throw new Error("Recipe not object");
+
+    const recipe = {
+      ...rawRecipe,
+      slug: name.replace(/\.ya?ml$/, ""),
+    };
+
+    const validationResult = recipeSchema.validate(recipe);
+    if (validationResult.error) throw validationResult.error;
+
+    return validationResult.value;
+  } catch (e) {
+    console.log(`Error while reading recipe: "${name}" from disc.`, e);
+  }
+};
+
+const recipes = fileNames
+  .map(readRecipeFromFile)
+  .filter((recipe): recipe is Recipe => !!recipe);
 
 export const getRecipes = () => recipes;
 
