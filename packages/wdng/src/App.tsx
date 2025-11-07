2d2e2cdf97a35826da0ab1a40bd9642d08c5cc11
Viktor Zetterström <viktor.zetterstrom@sanalabs.com>
2025-11-07 08:57:18 +0100

    remove unused fonts and add image

--- packages/wdng/src/App.tsx
+++ packages/wdng/src/App.tsx
@@ -1,9 +1,10 @@
 function App() {
   return (
     <div className="min-h-screen bg-gray-50 p-8">
-      <h1 className="font-parisienne text-6xl text-gray-900">WDNG</h1>
-      <p className="font-garamond mt-4 text-xl text-gray-700">Welcome to WDNG</p>
-      <div className="mt-8 space-y-4">
+      <img width={300} alt="Kvarnfallet" src="/kvarnfallet-outline.png" />
+      <h1 className="font-parisienne text-6xl text-gray-900">Välkommen på vigsel och fest</h1>
+      <p className="font-garamond mt-4 text-xl text-gray-700">Viktor & Hanna</p>
+      <div className="mt-1">
         <p className="font-parisienne text-3xl">Parisienne Font Example</p>
         <p className="font-garamond text-lg">EB Garamond Font Example</p>
         <p className="font-garamond text-lg italic">EB Garamond Italic Example</p>
